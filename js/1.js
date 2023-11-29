325e1563b8ae65f685d46f47300451797cb84389
gitcat <fotocat33@yandex.ru>
2023-11-30 00:46:35 +0300

    13

--- js/1.js
+++ js/1.js
@@ -22,14 +22,19 @@ $(document).ready(function(){
  		$('.uslugi__text__none').slideToggle(300, function(){
 			$('.uslugi__text__none').addClass('.uslugi__text__block');
 		})
-		var Pr1=$('#r1').hide();
-		var Pr2=$('.uslugi__img__x').show();
+		var Pr1=$('#r1')
+		Pr1.hide();
+		var Pr2=$('.uslugi__img__x')
+		Pr2.show(500);
 	})
 	$('.uslugi__img__x').click(function(){
 		$('.uslugi__text__none').slideToggle(300, function(){
 		$('.uslugi__text__none').removeClass('.uslugi__text__block');
 		})
-		Pr1=$('#r1').show();
-		Pr2=$('.uslugi__img__x').hide();
+		Pr2=$('.uslugi__img__x')
+		Pr1=$('#r1')
+		Pr2.hide();
+		Pr1.show();
+		
 	})
 })
