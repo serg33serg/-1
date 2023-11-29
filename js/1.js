9dd8a421c61802aa83377099110bdb57f1a5f8c9
gitcat <fotocat33@yandex.ru>
2023-11-29 16:44:22 +0300

    12

--- js/1.js
+++ js/1.js
@@ -1,4 +1,5 @@
-// Скрытый блок 1
+// КАТАЛОГ
+//блок 1
 $(document).ready(function(){
 	$('.content_toggle').click(function(){
 		$('.content_block').slideToggle(300, function(){
@@ -14,45 +15,21 @@ $(document).ready(function(){
 	});
 });
 
-
-// Скрытый блок 2
-// $(function(){
-// 	$('.uslugi__img__rectangle').click(function(){
-// 		$('.uslugi__text__none').slideToggle(300, function(){
-// 			if ($(this).is(':hidden')) {
-// 				console.log('1');
-// 				$('.uslugi__img__x').addClass('.img__x__block');
-// 				var Pr1=$('.uslugi__img__rectangle').hide();
-// 				var Pr2=$('.uslugi__img__x').show();
-// 				console.log(Pr1);
-//                 $('.uslugi__img__rectangle').addClass('.img__rectangle__none');
-// 				   Pr1=$('.uslugi__img__rectangle')
-// 				console.log(Pr1);
-			
-//                 $('.uslugi__text__none').addClass('.uslugi__text__block');
-// 			} else {
-//                 $('.uslugi__text__none').removeClass('.uslugi__text__block');
-// 				$('.uslugi__img__x').removeClass('.img__x__block');
-// 				$('.uslugi__img__rectangle').removeClass('.img__rectangle__none');
-// 				console.log('2');
-// 			}							
-// 		});
-// 		return false;
-// 	});
-// });
+// УСЛУГИ
+// блок 1
  $(function(){
- 	$('.uslugi__img__rectangle').click(function(){
+ 	$('#r1').click(function(){
  		$('.uslugi__text__none').slideToggle(300, function(){
 			$('.uslugi__text__none').addClass('.uslugi__text__block');
 		})
-		var Pr1=$('.uslugi__img__rectangle').hide();
+		var Pr1=$('#r1').hide();
 		var Pr2=$('.uslugi__img__x').show();
 	})
 	$('.uslugi__img__x').click(function(){
 		$('.uslugi__text__none').slideToggle(300, function(){
 		$('.uslugi__text__none').removeClass('.uslugi__text__block');
 		})
-		Pr1=$('.uslugi__img__rectangle').show();
+		Pr1=$('#r1').show();
 		Pr2=$('.uslugi__img__x').hide();
 	})
 })
